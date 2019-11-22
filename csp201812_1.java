8a2d77748bc725675b48316238df48930e1ea1a0
SikongMing <sikong_ming@aliyun.com>
2019-11-22 19:54:36 +0800

    临时

---
+++ csp201812_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class csp201812_1{
+public static void main(String[] args)
+{
+    int r;
+    int y;
+    int g;
+    int n;
+    Scanner scan = new Scanner(System.in);
+    r = scan.nextInt();
+    y = scan.nextInt();
+    g = scan.nextInt();
+    n = scan.nextInt();
+}
+}
